c1ff3345d74860538381d3054a49565b16b24b01
Brandon Lisonbee <blisonbee213596@gmail.com>
2023-10-11 12:24:58 -0600

    misc layout stuff

---
+++ src/components/Header.tsx
@@ -0,0 +1,14 @@
+export default function Header() {
+    return (
+        <header>
+            <nav>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#"></a></li>
+                    <li><a href="#"></a></li>
+                    <li><a href="#"></a></li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
---
+++ src/components/Person.tsx
@@ -0,0 +1,9 @@
+export default function Person() {
+    return (
+        <section className="grid">
+            <span>Name: Bill Smith</span>
+            <span>City: Rexburg</span>
+            <span>State: Idaho</span>
+        </section>
+    )
+}
