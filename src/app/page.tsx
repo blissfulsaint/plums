3e9ebbe8f0974ffe68ddc74004c5a06ce0761496
aachee246 <ach18003@byui.edu>
2023-10-21 23:49:29 -0600

    added cards and commented out global body css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+import Card from '../components/Card';
+
 export default function Home() {
   return (
     <main>
       <h1 className="m-5 text-cyan-200">Hello World!</h1>
+      <Card title="Card 1" content="This is the content of Card 1" />
+      <Card title="Card 2" content="This is the content of Card 2" />
     </main>
   );
 }
