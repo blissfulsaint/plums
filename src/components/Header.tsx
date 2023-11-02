2f7f230567a7eca62078260e30c2944c6cef6837
aachee246 <ach18003@byui.edu>
2023-11-01 20:15:34 -0600

    material-ui card and styling done

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,10 @@
 export default function Header() {
     return (
-        <header>
+        <header >
             <nav>
-                <ul>
-                    <li><a href="#">Home</a></li>
-                    <li><a href="#"></a></li>
-                    <li><a href="#"></a></li>
-                    <li><a href="#"></a></li>
+                <ul className="flex flex-row items-center pt-8 pr-8 pl-8">
+                    <li><a className="pr-4" href="#">Home</a></li>
+                    <li><a className="pr-4" href="#">Add New</a></li>
                 </ul>
             </nav>
         </header>
